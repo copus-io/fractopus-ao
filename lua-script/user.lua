76e6c591b5f22f77db3e1d9cc9358fcf3be8fa3e
john <john@server31.io>
2024-07-03 17:23:52 +0800

    getClient

--- lua-script/user.lua
+++ lua-script/user.lua
@@ -1,6 +1,9 @@
 local crypto = require(".crypto");
 local json = require('json')
 
+
+
+-- Send({Target=ao.id,Action= "addClient",Tags={email="1232@163.com"}})
 Handlers.add("addClient",
   Handlers.utils.hasMatchingTag("Action", "addClient"),
   function(msg)
@@ -16,24 +19,26 @@ Handlers.add("addClient",
     })
   end)
 
+
+-- Send({Target=ao.id,Action= "getClient",Tags={email="1232@163.com"}})
 Handlers.add("getClient",
   Handlers.utils.hasMatchingTag("Action", "getClient"),
   function(msg)
     assert(string.len(msg.Tags.email) > 0, "email is empty")
     local email = msg.Tags.email
-    local md5 = crypto.digest.md5(crypto.utils.stream.fromString(email)).asHex()
-
-    if Clients[md5] ~=nil then
+    local temp = crypto.utils.stream.fromString(email)
+    local md5 = crypto.digest.md5(temp).asHex()
+    if Clients[md5] ~= nil then
       ao.send({
         Target = msg.From,
         Action = 'getClientResp',
         Data = Clients[md5],
       })
-      else
-        ao.send({
-          Target = msg.From,
-          Action = 'getClientResp-Error',
-          Data = "not exist",
-        })
+    else
+      ao.send({
+        Target = msg.From,
+        Action = 'getClientResp-Error',
+        Data = "not exist",
+      })
     end
   end)
